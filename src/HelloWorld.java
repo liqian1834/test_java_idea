6fecb871bc9371b2776542995d9632c6b0757953
liqian <183472704@qq.com>
2018-10-09 16:20:28 +0800

    add 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,4 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+
+    private void Test(){
+        System.out.println("I'm casey");
+    }
 }
